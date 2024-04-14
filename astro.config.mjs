f094f5394f9fdebd9720f2e9cd4b0e623cbef4ab
Markku Virtanen <markku.virtanen@inderes.fi>
2024-04-14 22:40:39 +0300

    build: update packages, add qwik deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import qwikdev from "@qwikdev/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [db(), tailwind()]
+  integrations: [db(), tailwind(), qwikdev()]
 });
